fe91f50bec51cc13251d096bfb81b10801fc95d6
Adnan Muhib <adnan.muhib@rocketmail.com>
2017-11-27 05:56:07 +0500

    Initialize Empty Project ASP.net MVC Template

---
+++ CloudSchool/CloudSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CloudSchool.Startup))]
+namespace CloudSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
